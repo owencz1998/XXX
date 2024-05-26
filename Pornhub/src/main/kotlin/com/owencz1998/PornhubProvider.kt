16983a4ed4905f3c53df0792d9ea83ad8b411aa8
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-26 14:02:15 +0100

    Update PornhubProvider.kt

--- Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
+++ Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
@@ -30,7 +30,7 @@ class CornHubProvider : MainAPI() {
         "${mainUrl}/video?o=tr&t=w&hd=1&page="       to "Top Rated",
         "${mainUrl}/video?o=mv&t=w&hd=1&page="       to "Most Viewed",
         "${mainUrl}/video?o=ht&t=w&hd=1&page="       to "Hottest",
-        "${mainUrl}/video? m=q-1656TbeqM48zjadOf=w&hd=1&page="       to "Teen",
+        "/categories/teen" to "Teen",
         "${mainUrl}/video?p=professional&hd=1&page=" to "Professional",
         "${mainUrl}/video?o=lg&hd=1&page="           to "Longest",
         "${mainUrl}/video?p=homemade&hd=1&page="     to "Homemade",
