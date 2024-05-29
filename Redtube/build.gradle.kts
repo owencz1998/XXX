c010ab14817e03c89d7303857b5c3a650f081367
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-29 08:35:26 +0100

    Update build.gradle.kts

--- Redtube/build.gradle.kts
+++ Redtube/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 3 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://redtube.org&size=16"
 }
