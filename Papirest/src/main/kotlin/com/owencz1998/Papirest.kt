4a0273466822d803a2e38f8903ab4b1aa6474bd7
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-30 21:14:00 +0100

    Update Papirest.kt

--- Papirest/src/main/kotlin/com/owencz1998/Papirest.kt
+++ Papirest/src/main/kotlin/com/owencz1998/Papirest.kt
@@ -1,49 +1,60 @@
 package com.owencz1998
 
-import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.utils.Qualities
 
-class papirest : MainAPI() {
-    override var mainUrl              = "https://www.papi.rest"
-    override var name                 = "Papirest"
-    override val hasMainPage          = true
-    override var lang                 = "en"
-    override val hasQuickSearch       = false
-    override val hasDownloadSupport   = true
-    override val supportedTypes       = setOf(TvType.NSFW)
-    override val vpnStatus            = VPNStatus.MightBeNeeded
+class Papirest : MainAPI() {
+    override var mainUrl = "https://www.papi.rest/"
+    override var name = "Papirest"
+    override val hasMainPage = true
+    override val hasDownloadSupport = true
+    override val vpnStatus = VPNStatus.MightBeNeeded
+    override val supportedTypes = setOf(TvType.NSFW)
 
     override val mainPage = mainPageOf(
-        "category/onlyfans" to "Onlyfans",
-        "category/femdom" to "Femdom",
-        "category/asmr" to "ASMR",
-        "category/patreon" to "Patreon",
-        "category/manyvids" to "Random",
-        "category/tiktok" to "Tiktok",
-        "category/webcam" to "Webcam",
-        "category/snapchat" to "Snapchat",
+        "$mainUrl/new.html?page=" to "New Videos",
+        "$mainUrl/new.html?sm=trending&page=" to "Trending",
+        "$mainUrl/new.html?sm=views&page=" to "Most Viewed",
+        "$mainUrl/popular/top-viewed.html?p=day" to "Popular - Day",
+        "$mainUrl/popular/top-viewed.html" to "Popular - Week",
+        "$mainUrl/popular/top-viewed.html?p=month" to "Popular - Month",
+        "$mainUrl/popular/top-viewed.html?p=all" to "Popular - All Time"
     )
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/${request.data}/page/$page/").document
-        val home     = document.select("article").mapNotNull { it.toSearchResult() }
+    override suspend fun getMainPage(
+        page: Int, request: MainPageRequest
+    ): HomePageResponse {
+        var pageStr = ((page - 1) * 30).toString()
 
+        val document = if ("page=" in request.data) {
+            app.get(request.data + pageStr).document
+        } else if ("/blog/" in request.data) {
+            pageStr = ((page - 1) * 20).toString()
+            app.get(request.data.replace(".html", "$pageStr.html")).document
+        } else {
+            app.get(request.data.replace(".html", ".html/$pageStr")).document
+        }
+        val home = document.select("div.main_content div.post_el_small").mapNotNull {
+                it.toSearchResult()
+            }
         return newHomePageResponse(
-            list    = HomePageList(
-                name               = request.name,
-                list               = home,
-                isHorizontalImages = true
-            ),
-            hasNext = true
+            list = HomePageList(
+                name = request.name, list = home, isHorizontalImages = true
+            ), hasNext = true
         )
     }
 
-    private fun Element.toSearchResult(): SearchResponse {
-        val title     = this.select("header > h2 > a").text().trim()
-        val href      = fixUrl(this.select("header > h2 > a").attr("href"))
-        val posterUrl = fixUrlNull(this.select("header > a > img").attr("data-src"))
-        println(posterUrl)
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title = this.selectFirst("div.post_text")?.text() ?: return null
+        val href = fixUrl(this.selectFirst("a.js-pop")!!.attr("href"))
+        var posterUrl = fixUrl(this.select("div.vid_container div.post_vid_thumb img").attr("src"))
+        if (posterUrl == "") {
+            posterUrl =
+                fixUrl(this.select("div.vid_container div.post_vid_thumb img").attr("data-src"))
+        }
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
         }
@@ -51,52 +62,84 @@ class papirest : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
-
-        for (i in 1..5) {
-            val document = app.get("${mainUrl}/page/$i/?s=$query&id=5036").document
-
-            val results = document.select("article").mapNotNull { it.toSearchResult() }
-
+        for (i in 0 until 15) {
+            val document = app.get(
+                "$mainUrl/${query.replace(" ", "-")}.html?page=${i * 30}"
+            ).document
+            val results = document.select("div.main_content div.post_el_small").mapNotNull {
+                    it.toSearchResult()
+                }
             if (!searchResponse.containsAll(results)) {
                 searchResponse.addAll(results)
             } else {
                 break
             }
-
             if (results.isEmpty()) break
         }
-
         return searchResponse
     }
 
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
+        val title = document.selectFirst("div.post_text")?.text()?.trim().toString()
+        val poster = fixUrlNull(
+            document.selectFirst("meta[property=og:image]")
+                ?.attr("content")
+        )
 
-        val title       = document.selectFirst("meta[property=og:title]")?.attr("content")?.trim().toString()
-        val poster      = fixUrlNull(document.selectFirst("[property='og:image']")?.attr("content"))
-        val description = document.selectFirst("meta[property=og:description]")?.attr("content")?.trim()
-
+        val recommendations = document.select("div.main_content div div.post_el_small").mapNotNull {
+            it.toSearchResult()
+        }
 
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
             this.posterUrl = poster
-            this.plot      = description
+            this.recommendations = recommendations
         }
     }
 
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val document = app.get(data).document
-        //val sources = mutableListOf<String>()
-        document.select("article > div > div > button").forEach { button ->
-            val onclickAttr = button.attr("onclick")
-            val regex = Regex("""playEmbed\('([^']+)'\)""")
-            val matchResult = regex.find(onclickAttr)
-            val playlistUrl = matchResult?.groups?.get(1)?.value
-            playlistUrl?.let {
-                //println(it) // Print the extracted URL
-                //sources.add(it) // Add the URL to the sources list
-                loadExtractor(it, subtitleCallback, callback)
-            }
+    private fun updateUrl(arg: MutableList<String>): MutableList<String> {
+        arg[5] =
+            (Integer.parseInt(arg[5]) - (generateNumber(arg[6]) + generateNumber(arg[7]))).toString()
+        return arg
+    }
+
+    private fun generateNumber(arg: String): Int {
+        val str = arg.replace(Regex("\\D"), "")
+        var sut = 0
+        for (element in str) {
+            sut += Integer.parseInt(element.toString(), 10)
         }
+        return sut
+    }
+
+   override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = app.get(data).document
+        val parsed = AppUtils.parseJson<Map<String, String>>(
+            document.select("span.vidsnfo").attr("data-vnfo")
+        )
+        parsed[parsed.keys.toList()[0]]
+        var url = parsed[parsed.keys.toList()[0]].toString()
+
+        var tmp = url.split("/").toMutableList()
+        tmp[1] += "8"
+        tmp = updateUrl(tmp)
+
+        url = fixUrl(tmp.joinToString("/"))
+
+        callback.invoke(
+            ExtractorLink(
+                this.name,
+                this.name,
+                url,
+                referer = "",
+                quality = Qualities.Unknown.value
+            )
+        )
         return true
     }
 }
