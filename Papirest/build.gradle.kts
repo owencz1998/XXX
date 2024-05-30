c7d8e7c54fa1977fa10e54c18f974204f8a25df9
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-30 15:35:28 +0100

    fix

---
+++ Papirest/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 21
+
+cloudstream {
+    authors     = listOf("OwenConnor")
+    language    = "en"
+    description = "Redtube"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://ei.rdtcdn.com/www-static/cdn_files/redtube/icons/favicon.png?v=46b6baae4334d3e6961c561e501e5665dcee97a5"
+}
