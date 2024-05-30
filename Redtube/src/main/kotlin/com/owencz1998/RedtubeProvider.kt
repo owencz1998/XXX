e63207422af2eec3b2a9cf094452a35cb5097738
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-30 15:47:04 +0100

    Update RedtubeProvider.kt

--- Redtube/src/main/kotlin/com/owencz1998/RedtubeProvider.kt
+++ Redtube/src/main/kotlin/com/owencz1998/RedtubeProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Redtube: Plugin() {
+class RedtubeProvider: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Redtube())
     }
