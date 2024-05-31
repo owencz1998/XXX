9dfde7fea104da5e4ae24cf59b944b2ea4569a51
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-31 19:17:14 +0100

    Update XvideosProvider.kt

--- Xvideos/src/main/kotlin/com/owencz1998/XvideosProvider.kt
+++ Xvideos/src/main/kotlin/com/owencz1998/XvideosProvider.kt
@@ -1,17 +1,17 @@
 package com.owencz1998
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.api.Log
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.extractors.Filesim
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import com.lagradost.cloudstream3.utils.ExtractorApi
+import android.util.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.mvvm.logError
+import com.lagradost.cloudstream3.network.WebViewResolver
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.M3u8Helper
-import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.getQualityFromName
+import org.json.JSONObject
 
 class XvideosProvider : MainAPI() {
     private val globalTvType = TvType.NSFW
