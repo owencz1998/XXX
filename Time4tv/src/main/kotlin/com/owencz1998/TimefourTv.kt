1788f34683bb516a08a027dbda900f5e7ec34555
Owen <96526481+owencz1998@users.noreply.github.com>
2024-06-02 07:42:23 +0100

    Update TimefourTv.kt

--- Time4tv/src/main/kotlin/com/owencz1998/TimefourTv.kt
+++ Time4tv/src/main/kotlin/com/owencz1998/TimefourTv.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class TimefourTv : MainAPI() {
-    override var mainUrl = "https://dlhd.so/"
+    override var mainUrl = "https://time4tv.online/"
     override var name = "Time4tv"
     override val hasDownloadSupport = false
     override val hasMainPage = true
