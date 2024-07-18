8124715d5493b1f944c38340ac82dfa0fb1cf47c
Owen <96526481+owencz1998@users.noreply.github.com>
2024-07-18 22:49:33 +0100

    Update utils.kt

--- Porntube/src/main/kotlin/com/owencz1998/utils.kt
+++ Porntube/src/main/kotlin/com/owencz1998/utils.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class PirnforcePlugin: Plugin() {
+class PornforcePlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Pornforce())
     }
