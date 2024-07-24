7f9fe1a38afe283bff5e80315b6dc52200e76db4
Owen <96526481+owencz1998@users.noreply.github.com>
2024-07-24 20:15:48 +0100

    Update Mangoporn

--- Mangoporn/src/main/kotlin/com/Mangoporn/Mangoporn.kt
+++ Mangoporn/src/main/kotlin/com/Mangoporn/Mangoporn.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
 class Mangoporn : MainAPI() {
-    override var mainUrl              = "http://mangoporn.net"
+    override var mainUrl              = "http://mangoporn.co"
     override var name                 = "Mangoporn"
     override val hasMainPage          = true
     override var lang                 = "en"
