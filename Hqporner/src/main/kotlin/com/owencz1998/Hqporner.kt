7dc0b4e1bd1deed263c149445c575e45bfdb9268
Owen <96526481+owencz1998@users.noreply.github.com>
2024-08-13 09:51:32 +0100

    Update Hqporner.kt

--- Hqporner/src/main/kotlin/com/owencz1998/Hqporner.kt
+++ Hqporner/src/main/kotlin/com/owencz1998/Hqporner.kt
@@ -2,32 +2,37 @@ package com.owencz1998
 
 import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.network.WebViewResolver
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import org.jsoup.Jsoup
+import com.lagradost.cloudstream3.utils.*
 
-class Hqporner : MainAPI() {
+class Eporner : MainAPI() {
     override var mainUrl              = "https://m.hqporner.com"
     override var name                 = "Hqporner"
     override val hasMainPage          = true
     override var lang                 = "en"
-    override val hasQuickSearch       = false
     override val hasDownloadSupport   = true
     override val hasChromecastSupport = true
     override val supportedTypes       = setOf(TvType.NSFW)
     override val vpnStatus            = VPNStatus.MightBeNeeded
 
     override val mainPage = mainPageOf(
-        "${mainUrl}/home/"                to "Featured",
-        "${mainUrl}/category/amateur/"    to "Amateur",
-        "${mainUrl}/category/teen/"       to "Teen",
-        "${mainUrl}/category/anal-sex-hd/"    to "Anal",
-    )
+            "best-videos" to "Best Videos",
+            "top-rated" to "Top Rated",
+            "most-viewed" to "Most Viewed",
+            "cat/milf" to "Milf",
+            "cat/japanese" to "Japanese",
+            "cat/hd-1080p" to "1080 Porn",
+            "cat/4k-porn" to "4K Porn",
+            "cat/babysitter" to "Baby Sitter",
+            "cat/big-tits" to "Big Tits",
+            "cat/teen" to "Teen"
+
+
+        )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("${request.data}${page}").document
-        val home     = document.select("div.video-block div.video-card").mapNotNull { it.toSearchResult() }
+        val document = app.get("$mainUrl/${request.data}/$page/").document
+        val home     = document.select("#vidresults div.mb").mapNotNull { it.toSearchResult() }
 
         return newHomePageResponse(
             list    = HomePageList(
@@ -39,23 +44,29 @@ class Hqporner : MainAPI() {
         )
     }
 
-    private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("div.video-card div.video-card-body div.video-title a")?.text() ?: return null
-        val href = fixUrl(this.selectFirst("div.video-card div.video-card-body div.video-title a")!!.attr("href"))
-        val posterUrl = fixUrlNull(this.select("div.video-card div.video-card-image a img").attr("data-src"))
+    private fun Element.toSearchResult(): SearchResponse {
+        val title     = fixTitle(this.select("div.mbunder p a").text()).trim()
+        val href      = fixUrl(this.select("div.mbcontent a").attr("href"))
+        val posterUrl = fixUrlNull(this.select("div.mbcontent a img").attr("src"))
 
-        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+        }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
 
-        for (i in 1..15) {
-            val document = app.get("${mainUrl}/search?q=${query.replace(" ", "+")}&p=$i").document
+        for (i in 1..10) {
+            val document = app.get("${mainUrl}/search/$query/$i").document
 
-            val results = document.select("div.video-block div.video-card").mapNotNull { it.toSearchResult() }
+            val results = document.select("#vidresults div.mb").mapNotNull { it.toSearchResult() }
 
-            searchResponse.addAll(results)
+            if (!searchResponse.containsAll(results)) {
+                searchResponse.addAll(results)
+            } else {
+                break
+            }
 
             if (results.isEmpty()) break
         }
@@ -66,93 +77,43 @@ class Hqporner : MainAPI() {
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
 
-        val title     = document.selectFirst("div.video-block div.single-video-left div.single-video-title h2")?.text()?.trim().toString()
-        val iframeUrl = fixUrlNull(document.selectFirst("div.video-block div.single-video-left div.single-video iframe")?.attr("src")) ?: ""
+        val title       = document.selectFirst("meta[property=og:title]")?.attr("content")?.trim().toString()
+        val poster      = fixUrlNull(document.selectFirst("[property='og:image']")?.attr("content"))
+        val description = document.selectFirst("meta[property=og:description]")?.attr("content")?.trim()
 
-        val poster: String?
-        val posterHeaders: Map<String, String>
-        if (iframeUrl.contains("videoh")) {
-            val iframeDocument = app.get(iframeUrl, interceptor = WebViewResolver(Regex("""mydaddy"""))).document
-
-            val videoHtml = iframeDocument.selectXpath("//script[contains(text(),'poster')]").first()?.html()?.substringAfter("else{ \$(\"#jw\").html(\"")?.substringBefore("\");}if(hasAdblock)")?.replace("\\", "")
-            val video     = Jsoup.parse(videoHtml.toString()).selectFirst("video")
-
-            poster        = fixUrlNull(video?.attr("poster"))
-            posterHeaders = mapOf(Pair("referer", "https://mydaddy.cc/"))
-        } else {
-            val iframeDocument = app.get(iframeUrl).document
-            val videoDocument  = Jsoup.parse("<video" + iframeDocument.selectXpath("//script[contains(text(),'\$(\"#jw\").html(')]")[0]?.toString()?.replace("\\", "")?.substringAfter("<video")?.substringBefore("</video>") + "</video>")
-
-            poster        = fixUrlNull(videoDocument.selectFirst("video")?.attr("poster").toString())
-            posterHeaders = mapOf(Pair("referer", "https://xiaoshenke.net/"))
-        }
-
-        val tags            = document.select("div.video-blockdiv.single-video-left div.single-video-title p.tag-link span a").map { it.text() }
-        val description     = document.selectFirst("div.video-block div.single-video-left div.single-video-title h2")?.text()?.trim().toString()
-        val actors          = document.select("div.video-block div.single-video-left div.single-video-info-content p a").map { it.text() }
-        val recommendations = document.select("div.video-block div.video-recommendation div.video-card").mapNotNull { it.toSearchResult() }
 
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
-            this.posterUrl       = poster
-            this.posterHeaders   = posterHeaders
-            this.plot            = description
-            this.tags            = tags
-            this.recommendations = recommendations
-            addActors(actors)
+            this.posterUrl = poster
+            this.plot      = description
         }
     }
 
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val document    = app.get(data).document
-
-        val iframeUrl   = fixUrlNull(document.selectFirst("div.video-block div.single-video-left div.single-video iframe")?.attr("src")) ?: ""
-
-        val extlinkList = mutableListOf<ExtractorLink>()
-        if (iframeUrl.contains("videoh")) {
-            val iframeDocument = app.get(iframeUrl, interceptor = WebViewResolver(Regex("""mydaddy"""))).document
-            val videoDocument  = Jsoup.parse("<video" + iframeDocument.selectXpath("//script[contains(text(),'\$(\"#jw\").html(')]").first()?.toString()?.replace("\\", "")?.substringAfter("<video")?.substringAfter("<video")?.substringBefore("</video>") + "</video>")
-
-            videoDocument.select("source").map { res -> 
-                extlinkList.add(ExtractorLink(
-                    name,
-                    name,
-                    fixUrl(res.attr("src")),
-                    referer = data,
-                    quality = Regex("(\\d+.)").find(res.attr("title"))?.groupValues?.get(1).let { getQualityFromName(it) }
-                )) 
-            }
-        } else if (iframeUrl.contains("xiaoshenke")) {
-            val iframeDocument = app.get(iframeUrl).document
-            val videoID        = Regex("""var id = \"(.+?)\"""").find(iframeDocument.html())?.groupValues?.get(1)
-
-            val pornTrexDocument = app.get("https://www.porntrex.com/embed/${videoID}").document
-            val video_url = fixUrlNull(Regex("""video_url: \'(.+?)\',""").find(pornTrexDocument.html())?.groupValues?.get(1))
-            if (video_url != null) {
-                extlinkList.add(ExtractorLink(
-                    name,
-                    name,
-                    video_url,
-                    referer = data,
-                    quality = Qualities.Unknown.value
-                ))
-            }
-        } else {
-            val iframeDocument = app.get(iframeUrl).document
-            val videoDocument  = Jsoup.parse("<video" + iframeDocument.selectXpath("//script[contains(text(),'\$(\"#jw\").html(')]").first()?.toString()?.replace("\\", "")?.substringAfter("<video")?.substringBefore("</video>") + "</video>")
-
-            videoDocument.select("source").map { res -> 
-                extlinkList.add(ExtractorLink(
-                    this.name,
-                    this.name,
-                    fixUrl(res.attr("src")),
-                    referer = mainUrl,
-                    quality = Regex("(\\d+.)").find(res.attr("title"))?.groupValues?.get(1).let { getQualityFromName(it) }
-                )) 
+        val response = app.get(
+            data, interceptor = WebViewResolver(Regex("""https://www\.m.hqporner\.com/xhr/video"""))
+        )
+        val json=response.text
+        val regex = Regex("labelShort\":\\s\"(.*?)\"|src\":\\s\"(.*)\"")
+        val matches = regex.findAll(json)
+        val srcList = mutableListOf<Pair<String, String>>()
+        for (match in matches) {
+            val labelShort = match.groupValues[1]
+            val src = match.groupValues[2]
+            srcList.add(labelShort to src)
+        }
+        srcList.forEach { (labelShort, src) ->
+            if (!src.contains(".php")and(labelShort.isEmpty())) {
+                callback.invoke(
+                    ExtractorLink(
+                        source = name,
+                        name = name,
+                        url = src,
+                        referer = "",
+                        quality = getQualityFromName(labelShort)
+                    )
+                )
             }
         }
-
-        extlinkList.forEach(callback)
-
         return true
     }
 }
