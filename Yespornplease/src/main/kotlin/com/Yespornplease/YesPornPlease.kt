8b29050659a9571283aa702eb91e61c5e2beacb4
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-12 12:01:18 +0100

    Update YesPornPlease.kt

--- Yespornplease/src/main/kotlin/com/Yespornplease/YesPornPlease.kt
+++ Yespornplease/src/main/kotlin/com/Yespornplease/YesPornPlease.kt
@@ -16,7 +16,7 @@ class YesPornPlease : MainAPI() {
 
     override val mainPage = mainPageOf(
         "${mainUrl}" to "Home",
-        "${mainUrl}/xnxx/sexmex/" to "Sexmex",
+        "${mainUrl}/sexmex/" to "Sexmex",
         "${mainUrl}/xnxx/small-tits/" to "Small Tits",
         "${mainUrl}/xnxx/teen/" to "Teen",
         "${mainUrl}/xnxx/threesome/" to "Threesome",
