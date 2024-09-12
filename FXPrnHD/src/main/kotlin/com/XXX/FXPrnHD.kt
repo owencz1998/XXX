0146a9d9e424405bba8cfcd57b875337fd7032fb
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-12 17:20:26 +0100

    Update FXPrnHD.kt

--- FXPrnHD/src/main/kotlin/com/XXX/FXPrnHD.kt
+++ FXPrnHD/src/main/kotlin/com/XXX/FXPrnHD.kt
@@ -55,7 +55,8 @@ class Fxprnhd : MainAPI() {
     private fun Element.toSearchResult(): SearchResponse? {
         val title = this.selectFirst("span.title")?.text() ?: return null
         val href = fixUrl(this.selectFirst("a")!!.attr("href"))
-        val posterUrl  = fixUrlNull(this.select("img").attr("src"))
+        val posterUrl = this.select("img").attr("src")
+        Log.d("posterUrl", posterUrl)
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
         }
