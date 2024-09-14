0d7f5b6cd55cb324f1c017bdaebd2c3050a83cb7
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-14 11:19:56 +0100

    Update build.gradle.kts

--- Pornhoarder/build.gradle.kts
+++ Pornhoarder/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://pornhoarder.org/img/logo.svg"
+            iconUrl = "https://www.google.com/s2/favicons?domain=pornhoarder.com&sz=%size%"
 }
