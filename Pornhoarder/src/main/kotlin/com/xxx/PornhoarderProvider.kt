e46cfc25da306c685796274938b10811e1fc16bc
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-17 06:58:16 +0100

    Update PornhoarderProvider.kt

--- Pornhoarder/src/main/kotlin/com/xxx/PornhoarderProvider.kt
+++ Pornhoarder/src/main/kotlin/com/xxx/PornhoarderProvider.kt
@@ -3,10 +3,14 @@ package com.xxx
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.Wishonly
 
 @CloudstreamPlugin
 class PornhoarderProvider: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(PornhoarderPlugin())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(Wishonly())
     }
 }
