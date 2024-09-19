36013fb170d4c8df66e3757d25e640d2d65d5da8
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-19 06:57:46 +0100

    Update NewsensationsProvider.kt

--- Newsensations/src/main/kotlin/com/owencz1998/NewsensationsProvider.kt
+++ Newsensations/src/main/kotlin/com/owencz1998/NewsensationsProvider.kt
@@ -1,13 +1,13 @@
 package com.xxx
 
 import android.content.Context
-import com.xxx.newsensations
+import com.xxx.Newsensations
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class NewsensationsProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(NewSensations())
+        registerMainAPI(Newsensations())
     }
 }
