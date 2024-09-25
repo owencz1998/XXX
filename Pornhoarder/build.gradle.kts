a49982725f8dffe579b6889239c8a504c8160d7a
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-25 07:14:22 +0100

    icon fixed maybe

--- Pornhoarder/build.gradle.kts
+++ Pornhoarder/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://imgur.com/download/bF7P7os/"
+    iconUrl = "https://www.google.com/s2/favicons?domain=pornhoarder.com&sz=%size%"
 }
