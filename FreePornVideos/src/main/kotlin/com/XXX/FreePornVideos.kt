5826796730e5655332248f432b7c5dacdb60e6c2
Owen <96526481+owencz1998@users.noreply.github.com>
2024-09-26 08:55:35 +0100

    Update FreePornVideos.kt

--- FreePornVideos/src/main/kotlin/com/XXX/FreePornVideos.kt
+++ FreePornVideos/src/main/kotlin/com/XXX/FreePornVideos.kt
@@ -149,7 +149,8 @@ class FreePornVideos : MainAPI() {
         "sites/trans-angels" to "Trans Angels",
         "sites/transfixed" to "TransFixed",
         "sites/pinko-tgirls" to "Pinko Tgirls",
-        "models" to "Pornstars",
+        "models/most-popular" to "Most Popular Pornstars",
+        "models/top-rated" to "Top Rated Pornstars",
         "models/angela-white" to "Angela White",
         "models/riley-reid" to "Riley Reid",
         "models/abella-danger" to "Abella-danger"
