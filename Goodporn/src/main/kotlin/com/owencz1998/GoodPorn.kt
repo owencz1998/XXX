5b40b0249446ace7f969851184901afe244a8dde
Owen <96526481+owencz1998@users.noreply.github.com>
2024-10-01 09:58:41 +0100

    Update GoodPorn.kt

--- Goodporn/src/main/kotlin/com/owencz1998/GoodPorn.kt
+++ Goodporn/src/main/kotlin/com/owencz1998/GoodPorn.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import java.util.*
 
 class GoodPorn : MainAPI() {
-    override var mainUrl              = "https://pornenix.com/"
+    override var mainUrl              = "https://www.fpo.xxx"
     override var name                 = "FPO"
     override val hasMainPage          = true
     override var lang                 = "en"
