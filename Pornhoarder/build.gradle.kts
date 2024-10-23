8b92c8903358a59fda5c11696952c0ed6518b679
Owen <96526481+owencz1998@users.noreply.github.com>
2024-10-23 10:44:20 +0100

    Update build.gradle.kts

--- Pornhoarder/build.gradle.kts
+++ Pornhoarder/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://pornhoarder.org/img/logo.svg"
+    iconUrl = "https://a.adtng.com/click/eyJleHRfemlkIjoiIiwiZXh0X2MiOiIiLCJleHRfYWlkIjoiIiwicGlkIjoiNDkiLCJzaWQiOiIxMDAxMzM3MSIsIm5pZHMiOiI2MjQyOSIsImR5bl9kbW4iOiIiLCJjcmlkIjoiMTExMjAxMyIsInN2IjoiNDc2IiwicmVmX2RtbiI6InN4eXBybi5jb20iLCJleHRfY2lkIjoiIiwidHNuYW1lIjoiTUIiLCJjcmMiOiI0IiwiY24iOiIzMDBYMjUwX01JWF9ERU0iLCJuaWQiOiI2MjQyOSIsImV4dF9wdWIiOiIiLCJjcnAiOiI2NC44MSIsInRpZCI6IjEiLCJpdCI6IjIzXC9PY3RcLzIwMjQ6MDk6NDM6MDEgKzAwMDAiLCJjYyI6IjEiLCJzbmNpZCI6IjEwMjA3MiIsImNpZCI6IjM2OTAxIiwiZXh0X3VpZCI6IiIsImNwIjoiMTAwIiwic25jY2lkIjoiMjQzMzY4NiIsImlpZCI6IjRmNWU4ZjIyZGQ4Nzc3NjkzZTEyYjg4M2M2MzdiYzFjIiwiZXh0X2lpZCI6IiJ9?lp=KTY3NDZyZmU%2FKz1%2FYTMhNy0iPEJLWFFGRlxHXBciLS5rMS85e3QtKREmNW5uZmFgAQgQVUZQDnxZTSQwLSUpOzEzOz4jaC4lMGtmaGkBCwUDA24BAQQKd3p1G3Z%2BcHp6E3x%2FfmNjZ2QHG2pnDgULCAIF"
 }
