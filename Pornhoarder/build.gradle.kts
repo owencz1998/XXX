2303d13e995a7d3ac2b22e621c99db8a1a3c34e3
Owen <96526481+owencz1998@users.noreply.github.com>
2024-10-23 09:19:13 +0100

    Update build.gradle.kts

--- Pornhoarder/build.gradle.kts
+++ Pornhoarder/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
-    iconUrl = "https://www.google.com/s2/favicons?domain=pornhoarder.com&sz=%size%"
+    iconUrl = "https://www.google.com/search?sca_esv=945b0f90c133864c&sxsrf=ADLYWIKimWDVP6DmBC6wbiyteP9H3U8SkA:1729671491786&q=pornhoarder&udm=2&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWwAFG7ranuZ26H8lR7pf_8AzBs6lnFFuPH6eU3OV27QKh6ftn9lc4yAcaBgSvqjbSCyeaApdzCHMvL4n82P-FwUG6xXTIl3AXBdTeyTsLpCulLlT-wwbW1VfYYLjNrqyC3Vy00mrhhTW85z0vcKkl-z7UtMxfYgFfPDrK7JW_z0ln1Etn&sa=X&ved=2ahUKEwjwieK2iKSJAxUFaEEAHQrGMSwQtKgLegQIDhAB&biw=400&bih=728&dpr=3.6#vhid=CR1hmt-4OR6PIM&vssid=mosaic"
 }
