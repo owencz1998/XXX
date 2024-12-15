2e01e1e1e4fdc44d439cbee9a3d00384fd1c884f
Owen <96526481+owencz1998@users.noreply.github.com>
2024-12-15 12:35:58 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
 
 buildscript {
     repositories {
         google()
         mavenCentral()
+        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
@@ -12,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.4")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
@@ -25,6 +26,7 @@ allprojects {
 }
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
@@ -33,8 +35,9 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // compatible with other git hosting services, like gitlab, gitDab, codeBerg
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/Phisher98")
+        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
+        // you can modify it to use other git hosting services, like gitlab
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/LikDev-256/likdev256-tamil-providers")
     }
 
     android {
@@ -47,7 +50,6 @@ subprojects {
         }
 
         compileOptions {
-            isCoreLibraryDesugaringEnabled = true
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
@@ -71,17 +73,23 @@ subprojects {
         // Stubs for all Cloudstream classes
         //noinspection GradleDeprecatedConfiguration
         apk("com.lagradost:cloudstream3:pre-release")
+
+        // these dependencies can include any of those which are added by the app,
+        // but you dont need to include any of them if you dont need them
+        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
-        implementation("org.jsoup:jsoup:1.18.1") // html parser
-        implementation("com.android.tools:desugar_jdk_libs:2.1.3")
-        //noinspection GradleDependency
+        implementation("org.jsoup:jsoup:1.17.2") // html parser
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-        implementation("com.faendir.rhino:rhino-android:1.6.0")
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.11.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
+        implementation("org.json:json:20231013")
+        implementation("com.faendir.rhino:rhino-android:1.6.0")
+        implementation("commons-codec:commons-codec:1.15")
+        implementation("com.github.mezhevikin:http-request-kotlin:0.0.5")
+        implementation("com.google.code.gson:gson:2.10")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+        implementation("com.google.cloud:google-cloud-translate:1.94.4")
     }
 }
 
