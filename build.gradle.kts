8951a6bd40b86b360b5ac1e624d96d257227ddf7
Owen <96526481+owencz1998@users.noreply.github.com>
2024-12-15 12:28:48 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
+import com.android.build.gradle.BaseExtension
+
 buildscript {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        // Cloudstream gradle plugin which makes everything work and builds plugins
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-        // Universal build config
-        classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.15.1")
     }
 }
 
@@ -17,11 +20,71 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        mavenLocal()
         maven("https://jitpack.io")
     }
 }
 
-//tasks.register<Delete>("clean") {
-//    delete(rootProject.layout.buildDirectory)
-//}
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        // compatible with other git hosting services, like gitlab, gitDab, codeBerg
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/Phisher98")
+    }
+
+    android {
+
+        defaultConfig {
+            minSdk = 21
+            compileSdkVersion(33)
+            //noinspection OldTargetApi
+            targetSdk = 33
+        }
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "1.8" // Required
+                // Disables some unnecessary features
+                freeCompilerArgs = freeCompilerArgs +
+                        "-Xno-call-assertions" +
+                        "-Xno-param-assertions" +
+                        "-Xno-receiver-assertions"
+            }
+        }
+    }
+
+    dependencies {
+        val apk by configurations
+        val implementation by configurations
+
+        // Stubs for all Cloudstream classes
+        //noinspection GradleDeprecatedConfiguration
+        apk("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
+        implementation("org.jsoup:jsoup:1.18.1") // html parser
+        implementation("com.android.tools:desugar_jdk_libs:2.1.3")
+        //noinspection GradleDependency
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+        implementation("com.faendir.rhino:rhino-android:1.6.0")
+        implementation("me.xdrop:fuzzywuzzy:1.4.0")
+        implementation("com.google.code.gson:gson:2.11.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
