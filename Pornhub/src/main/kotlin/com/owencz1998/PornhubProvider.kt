9fe27ef8dd460aa3ce6a97c645dbb1d01dec4d5c
Owen <96526481+owencz1998@users.noreply.github.com>
2025-02-02 00:44:11 +0000

    new test

--- Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
+++ Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
@@ -34,6 +34,7 @@ class CornHubProvider : MainAPI() {
         "${mainUrl}/video?o=lg&hd=1&page="           to "Longest",
         "${mainUrl}/video?p=homemade&hd=1&page="     to "Homemade",
         "${mainUrl}/video?o=cm&t=w&hd=1&page="       to "Newest",
+        "${mainUrl}/video?o=bj&t=w&hd=1&page="       to "Blowjob",
     )
     private val cookies = mapOf(Pair("hasVisited", "1"), Pair("accessAgeDisclaimerPH", "1"))
 
