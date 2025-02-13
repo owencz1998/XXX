928a61cfa962140c7e0bee0754baf5920a114848
Owen <96526481+owencz1998@users.noreply.github.com>
2025-02-13 19:20:48 +0000

    Update PornhubProvider

--- Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
+++ Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
@@ -46,7 +46,7 @@ class PornHubProvider : MainAPI() {
         "${mainUrl}/video?c=8&page="                 to "Big Tits",
         "${mainUrl}/video?c=13&page="                to "Blowjob", 
         "${mainUrl}/video?c=16&page="                to "Cumshot",
-        "${mainUrl}/video?c=76&page="                to "Bisexual Male",            
+         "${mainUrl}/video?c=96&page="                to "British",            
     )
     private val cookies = mapOf(Pair("hasVisited", "1"), Pair("accessAgeDisclaimerPH", "1"))
 
