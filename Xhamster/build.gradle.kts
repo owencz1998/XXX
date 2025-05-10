02ba66953ff8a203d0ab61ba8afb90533aa51058
Owen <96526481+owenconnorz@users.noreply.github.com>
2025-05-10 07:34:32 +0100

    Update

--- Xhamster/build.gradle.kts
+++ Xhamster/build.gradle.kts
@@ -13,6 +13,7 @@ cloudstream {
      * 3: Beta only
     **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("NSFW")
+    tvTypes = listOf("NSFW",
+                     "Movie")
     iconUrl = "https://www.google.com/s2/favicons?domain=xhamster.com&sz=%size%"
 }
